aeab202c122de2b5e43fb535ddedeecb30e04e11
Basenga01 <oleinikmaksim07@gmail.com>
2023-03-05 18:05:03 +0300

    создание базовой  структуры  главной  страницы

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import {Main} from "./page/main";
+
 
 function App() {
   return (
     <div className="App">
-
+     <Main/>
     </div>
   );
 }
