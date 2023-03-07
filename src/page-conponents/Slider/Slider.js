7cb0318212b7d95e0c109f08334240d985dd935c
Basenga01 <oleinikmaksim07@gmail.com>
2023-03-07 17:39:52 +0300

    наброски Slider.js

--- src/page-conponents/Slider/Slider.js
+++ src/page-conponents/Slider/Slider.js
@@ -1,6 +1,9 @@
 import styles from "./slider.module.css"
 export const Slider =()=> {
-    return <div className={styles.Main}>
-        Карусель
+    return <div className={styles.Navigation}>
+        <div className={styles.Button}>
+            <button className={styles.Strelka1}></button>
+            <button className={styles.Strelka2}></button>
+        </div>
     </div>
 }
