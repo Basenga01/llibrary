4f5024b8ea3539c8f2d21ff878e6a1658227b43b
Basenga01 <oleinikmaksim07@gmail.com>
2023-03-11 22:42:14 +0300

    настраиваю стили для блоков

--- src/Components/Personal/Personal.js
+++ src/Components/Personal/Personal.js
@@ -1,5 +1,7 @@
+import styles from ".//Personal.module.css"
 export const Personal =()=> {
-    return <div>
-        Персонал
+    return <div className={styles.PersonalContainer}>
+        <div className={styles.PersonalLeft}></div>
+        <div></div>
     </div>
 }
--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,5 +1,7 @@
+import styles from "./Video.modules.css"
 export const Video =()=> {
-    return <div>
-        Видео
+    const src = "https://youtu.be/Ixi0sUpLVRc"
+    return <div className={styles.Video}>
+    фцвфцвцфвцфвфц
     </div>
 }
